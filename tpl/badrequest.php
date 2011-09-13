ab4e64ec94c0cbdbbac2dc6bd24e8a81c60b8b81
Tiffany B. Brown <tiffany@webinista.com>
2011-09-13 16:58:27 -0700

    Adding tpl/badrequest.php

---
+++ tpl/badrequest.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="UTF-8">
+	<title>Bad Request</title>
+	<link rel="stylesheet" href="tpl/s.css">
+</head>
+<body>
+<h1><?php print $title; ?></h1>
+</body>
+</html>
